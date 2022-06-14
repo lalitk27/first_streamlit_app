e430e69363172dd4efab41c30b39e0cf6c9f641a
Lalit Kumar <96349860+lalitk27@users.noreply.github.com>
2022-06-14 15:27:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 
-streamlit.title('Title - my app output')
-streamlit.header('Header')
-streamlit.text('Text')
+streamlit.title('Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
